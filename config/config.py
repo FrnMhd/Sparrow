12636d3e1cf420b38d715a547f5d91e4f96a9d7c
FrnMhd <96841164+FrnMhd@users.noreply.github.com>
2022-04-16 20:42:29 +0530

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,4 @@
+api_key = "RSZjBFIKELSmycKKB9rvXLtp9"
+api_secrets = "IF1haNyRkHsaQb5tFtIcT2kpojmUfgYlslhLdys7J3m9qRvUQG"
+access_token = "1515311399878422542-9IuvjqLtrc7rqGgxgrHg3HTHT7LmrC"
+access_secret = "1N52oC1djy7Atrvf79KG2WRwDzbCD5sqkYfT8yiuUjtMV"
