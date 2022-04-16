35931fc4aeee4f1efd32ca45771728d3eb10ea54
FrnMhd <96841164+FrnMhd@users.noreply.github.com>
2022-04-16 20:43:25 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -2,3 +2,4 @@ api_key = "RSZjBFIKELSmycKKB9rvXLtp9"
 api_secrets = "IF1haNyRkHsaQb5tFtIcT2kpojmUfgYlslhLdys7J3m9qRvUQG"
 access_token = "1515311399878422542-9IuvjqLtrc7rqGgxgrHg3HTHT7LmrC"
 access_secret = "1N52oC1djy7Atrvf79KG2WRwDzbCD5sqkYfT8yiuUjtMV"
+API_KEY1 = 'fa82ecd32d13309b3edb6c5568627c6c6c2f4965'
